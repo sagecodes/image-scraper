b7c6be0f158ebdd680084efd34bf6638b5b2eecc
Sage Elliott <hello@sageelliott.com>
2019-09-10 21:26:46 -0700

    Able to Save images from url- VMP state

--- scrape.py
+++ scrape.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import urllib
+import urllib.request
 
+# Set Query | In future use Arg arse
 query = "pepsi"
-url =  f"https://www.google.co.in/search?q={query}&source=lnms&tbm=isch"
 
+# Set URL to google search
+url =  f"https://www.google.co.in/search?q={query}&source=lnms&tbm=isch"
+img_num = 1
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
@@ -14,16 +19,13 @@ for image in tags:
     #print image source
     img_url = image['src']
     img = requests.get(img_url, timeout=30)
-
-    # Define Path
-    p = ''
-    # p = os.path.sep.join([args["output"], "{}{}".format(
-	# 			str(total).zfill(8), ext)])
-    img_num = 1
-    p = os.path.join('/output')
+    print(image['src'])
+    p = os.path.join(f"output/{img_num}.jpg")
 
 
-    # Save File to Folder
-    folder = open(p, "wb")
-    folder.write(img)
-    folder.close()
+    #Save File to Folder
+    # f = open(f'{p}/{img_num}.jpg', "wb")
+    f = open(p, "wb")
+    f.write(img.content)
+    f.close()
+    img_num = img_num + 1
